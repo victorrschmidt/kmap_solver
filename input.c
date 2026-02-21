af44191729c1d57bbe4e4aacdfe5a822dcf62822
Victor Schmidt <vicoschmidt@gmail.com>
2026-02-21 00:31:37 -0300

    Att

--- input.c
+++ input.c
@@ -6,42 +6,47 @@
 #include <stdbool.h>
 #include <string.h>
 
+// Creates a Buffer struct with a specified size for char *content
 Buffer *createBuffer(size_t size) {
     Buffer *buffer = calloc(1, sizeof(Buffer));
     if (buffer == NULL) {
-        endProgram("Memory alloc error.");
+        endProgram(DEFAULT_ALLOCATION_ERROR_MESSAGE);
     }
     buffer->size = size;
     buffer->content = calloc(buffer->size, sizeof(char));
     if (buffer->content == NULL)  {
-        endProgram("Memory alloc error.");
+        endProgram(DEFAULT_ALLOCATION_ERROR_MESSAGE);
     }
     return buffer;
 }
 
-// ---------------------------------------------------------------------------
-
+// Creates a VariableSet struct with a specified size for bool **content
 VariableSet *createVariableSet(size_t size) {
     VariableSet *variable_set = malloc(sizeof(VariableSet));
     if (variable_set == NULL) {
-        endProgram("Memory alloc error.");
+        endProgram(DEFAULT_ALLOCATION_ERROR_MESSAGE);
     }
     variable_set->size = size;
     variable_set->content = malloc(variable_set->size * sizeof(bool*));
     if (variable_set->content == NULL) {
-        endProgram("Memory alloc error.");
+        endProgram(DEFAULT_ALLOCATION_ERROR_MESSAGE);
     }
     for (size_t i = 0; i < variable_set->size; i++) {
         variable_set->content[i] = calloc(26, sizeof(bool));
         if (variable_set->content[i] == NULL) {
-            endProgram("Memory alloc error.");
+            endProgram(DEFAULT_ALLOCATION_ERROR_MESSAGE);
         }
     }
     return variable_set;
 }
 
-// ---------------------------------------------------------------------------
+// Frees a Buffer struct
+void freeBuffer(Buffer *buffer) {
+    free(buffer->content);
+    free(buffer);
+}
 
+// Frees a VariableSet struct
 void freeVariableSet(VariableSet *variable_set) {
     for (size_t i = 0; i < variable_set->size; i++) {
         free(variable_set->content[i]);
@@ -50,19 +55,11 @@ void freeVariableSet(VariableSet *variable_set) {
     free(variable_set);
 }
 
-// ---------------------------------------------------------------------------
-
-void freeBuffer(Buffer *buffer) {
-    free(buffer->content);
-    free(buffer);
-}
-
-// ---------------------------------------------------------------------------
-
+// Reads a line from a text file and saves the content in the Buffer content
 void readInput(Buffer *buffer, const char *file_name) {
     FILE *input_file = fopen(file_name, "r");
     if (input_file == NULL) {
-        endProgram("Memory alloc error");
+        endProgram(DEFAULT_ALLOCATION_ERROR_MESSAGE);
     }
     fgets(buffer->content, buffer->size * sizeof(char), input_file);
     fclose(input_file);
@@ -72,8 +69,7 @@ void readInput(Buffer *buffer, const char *file_name) {
     }
 }
 
-// ---------------------------------------------------------------------------
-
+// Trims a Buffer content, removing all the spaces (ASCII 32)
 void removeSpaces(Buffer *buffer) {
     size_t non_space_count = 0;
     for (size_t i = 0; i < buffer->length; i++) {
@@ -99,13 +95,12 @@ void removeSpaces(Buffer *buffer) {
     buffer->size = non_space_count + 1;
     buffer->content = realloc(buffer->content, buffer->size * sizeof(char));
     if (buffer->content == NULL) {
-        endProgram("Memory alloc error");
+        endProgram(DEFAULT_ALLOCATION_ERROR_MESSAGE);
     }
     buffer->content[buffer->length] = '\0';
 }
 
-// ---------------------------------------------------------------------------
-
+// Validate a sum of products expression
 void validateExpression(Buffer *buffer) {
     removeSpaces(buffer);
     checkValidSumExpression(buffer);
@@ -117,8 +112,7 @@ void validateExpression(Buffer *buffer) {
     printf("OK!\n");
 }
 
-// ---------------------------------------------------------------------------
-
+// Checks if the Buffer content contains a valid sum between seperate expressions
 void checkValidSumExpression(Buffer *buffer) {
     if (buffer->content[0] == '+' || buffer->content[buffer->length - 1] == '+') {
         endProgram("Error: Invalid sum expression.\nCan't start and/or end with '+'.");
@@ -130,8 +124,7 @@ void checkValidSumExpression(Buffer *buffer) {
     }
 }
 
-// ---------------------------------------------------------------------------
-
+// Returns the amount of products contained in the Buffer content
 size_t getProductCount(Buffer *buffer) {
     size_t product_count = 0;
     for (size_t i = 0; i < buffer->length; i++) {
@@ -142,8 +135,7 @@ size_t getProductCount(Buffer *buffer) {
     return product_count + 1;
 }
 
-// ---------------------------------------------------------------------------
-
+// Checks if every product expression in the Buffer content is valid
 void checkValidProductExpressions(Buffer *buffer, VariableSet *variable_set) {
     size_t product_id = 0;
     size_t l = 0;
@@ -162,8 +154,7 @@ void checkValidProductExpressions(Buffer *buffer, VariableSet *variable_set) {
     }
 }
 
-// ---------------------------------------------------------------------------
-
+// Checks if a product expression is valid
 bool isValidProductExpression(VariableSet *set, size_t line, char *string, size_t l, size_t r) {
     bool reading_not = false;
     for (size_t i = l; i <= r; i++) {
@@ -184,8 +175,7 @@ bool isValidProductExpression(VariableSet *set, size_t line, char *string, size_
     return !reading_not;
 }
 
-// ---------------------------------------------------------------------------
-
+// Checks if all products share the same set of variables
 void checkEqualVariablesBetweenProducts(VariableSet *variable_set) {
     for (size_t i = 0; i < 26; i++) {
         bool pivot = variable_set->content[0][i];
@@ -196,5 +186,3 @@ void checkEqualVariablesBetweenProducts(VariableSet *variable_set) {
         }
     }
 }
-
-// ---------------------------------------------------------------------------
--- program.h
+++ program.h
@@ -1,5 +1,6 @@
 #ifndef PROGRAM_HEADER
 #define PROGRAM_HEADER
+#define DEFAULT_ALLOCATION_ERROR_MESSAGE "Memory allocation error."
 
 void endProgram(const char *message);
 
